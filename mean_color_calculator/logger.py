722ef08e5266e8f5e3ef4de66f5bacf85b087b1b
Adam Seman (1084076) <adam.seman@iqvia.com>
2021-08-03 08:44:06 +0200

    logging

---
+++ mean_color_calculator/logger.py
@@ -0,0 +1,11 @@
+import logging
+
+LOG_FORMAT = '{asctime} [{levelname}] [{filename}:{lineno}] {message}'
+
+logger = logging.getLogger('Image Class Calculation')
+formatter = logging.Formatter(LOG_FORMAT, datefmt='%Y-%m-%d %H:%M:%S', style='{')
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.INFO)
+logger.addHandler(stream_handler)
+logger.setLevel(logging.DEBUG)
